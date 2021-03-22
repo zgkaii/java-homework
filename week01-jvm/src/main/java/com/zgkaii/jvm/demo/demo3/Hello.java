156a268ef16c35003f08a59767c9f15aeb991c79
zgkaii <2488784449@qq.com>
2021-03-23 01:11:22 +0800

    update 2021/03/23

--- week01-jvm/src/main/java/com/zgkaii/jvm/demo/demo3/Hello.java
+++ week01-jvm/src/main/java/com/zgkaii/jvm/demo/demo3/Hello.java
@@ -2,6 +2,6 @@ package com.zgkaii.jvm.demo.demo3;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int i = 2 + 3;
     }
 }
---
+++ week01-jvm/src/main/java/com/zgkaii/jvm/demo6/ClassLoaderTest.java
@@ -0,0 +1,25 @@
+package com.zgkaii.jvm.demo6;
+
+import sun.security.ec.CurveDB;
+
+import java.net.URL;
+import java.security.Provider;
+
+/**
+ * @Author: Mr.Z
+ * @DateTime: 2021/03/22 21:41
+ * @Description:
+ **/
+public class ClassLoaderTest {
+    public static void main(String[] args) {
+        System.out.println("*********扩展类加载器************");
+        String extDirs = System.getProperty("java.ext.dirs");
+        for (String path : extDirs.split(";")) {
+            System.out.println(path);
+        }
+
+        // Java\lib\ext目录下随意选择一个类，查看他的类加载器是什么
+        ClassLoader classLoader = CurveDB.class.getClassLoader();
+        System.out.println(classLoader);
+    }
+}
